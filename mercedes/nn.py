ff49830d8a1fc5ad58b84b934413e2748a5f12e7
Deepak Labh <labh111349@gmail.com>
2017-06-25 13:49:31 +0530

    neural nets

--- mercedes/nn.py
+++ mercedes/nn.py
@@ -7,6 +7,14 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import normalize 
 from sklearn.decomposition import PCA
 from copy import deepcopy
+from sklearn.metrics import r2_score
+from keras.callbacks import ModelCheckpoint
+import model_arch as ma
+
+# evaluation metric
+def the_metric(y_pred, y):
+    y_true = y
+    return 'my_r2', r2_score(y_true, y_pred)
 
 train = pd.read_csv('data/train.csv')
 test = pd.read_csv('data/test.csv')
@@ -20,14 +28,17 @@ d_int_keys = list(set(d_int_keys).difference(set(d_t_str_keys)))
 data_str_test = test[d_t_str_keys]
 data_str_train = train[d_t_str_keys]
 #data_str.append(test[d_t_str_keys])
-unique_str_vals = list(set(list(set(data_str_test.values.ravel()))+ list(set(data_str_train.values.ravel()))))
+unique_str_vals = sorted(list(set(list(set(data_str_test.values.ravel()))+ list(set(data_str_train.values.ravel())))))
 lbl = LabelEncoder() 
 lbl.fit(unique_str_vals)
 
 for i in d_t_str_keys:
     train[i] = lbl.transform(train[i]) 
+
 train[d_t_str_keys] = train[d_t_str_keys] / np.linalg.norm(train[d_t_str_keys])
 train= train.iloc[np.random.permutation(len(train))]
+train_int_data = train[d_int_keys]
+test_int_data = test[d_int_keys]
 
 for i in d_t_str_keys:
     test[i] = lbl.transform(test[i]) 
@@ -38,27 +49,24 @@ test[d_t_str_keys] = test[d_t_str_keys] / np.linalg.norm(test[d_t_str_keys])
 pca = PCA(n_components=10, svd_solver='full')
 
 y_train = train['y']
-p_train = pca.fit_transform(train.drop(['y'], axis=1))
-p_test = pca.fit_transform(test)
+y_mean = y_train.mean()
+p_train = pca.fit_transform(train_int_data)
+p_train = np.concatenate((p_train,train[d_t_str_keys]), axis=1)
 
-train_size = int(0.8*len(p_train))
-dtrain = xgb.DMatrix( p_train[:train_size], label=y_train[:train_size])
-deval = xgb.DMatrix( p_train[train_size:], label=y_train[train_size:])
-dtest = xgb.DMatrix( p_test)
+p_test = pca.fit_transform(test_int_data)
+p_test = np.concatenate((p_test,test[d_t_str_keys]), axis=1)
 
-param = {'max_depth':10, 'eta':1, 'silent':1, 'objective':'reg:linear' }
-param['nthread'] = 4
-#param['eval_metric'] = 'auc'
-plst = param.items()
+train_size = int(0.9*len(p_train))
+dtrain = p_train[:train_size]
+label_train=y_train[:train_size]
+deval = p_train[train_size:]
+label_val=y_train[train_size:]
 
-evallist  = [(deval,'eval'), (dtrain,'train')]
-num_round = 2000
-bst = xgb.train( plst, dtrain, num_round, evallist, early_stopping_rounds=10 )
-bst.save_model('model/0001.model')
+print (p_train.shape)
+model = ma.fcnn(p_train.shape[1])
+history = model.fit(dtrain, label_train, validation_data=(deval, label_val), nb_epoch=100, batch_size=8, verbose=1)
 
-ypred = bst.predict(dtest)
-ID = test['ID']
+result = model.predict(deval)
 
-dic = {'ID':ID, 'y':ypred}
-df = pd.DataFrame(dic, columns=["ID",'y'])
-df.to_csv('data/Sample_submission_1.csv', index=False)
+r2_score = the_metric(result, label_val)
+print r2_score
